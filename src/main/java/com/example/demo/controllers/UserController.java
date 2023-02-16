a2ae17bf3c032b6a84980e0818e14e2465e7637e
MaryWagura <waguramarie@gmail.com>
2023-02-16 10:01:00 +0300

    change

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -3,7 +3,9 @@ package com.example.demo.controllers;
 import com.example.demo.db.UserRepository;
 import com.example.demo.db.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,7 +15,14 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
+    @Controller
+    public class JSONToXMLMappingController {
 
+        @PostMapping(path = "/mapJsontoXML", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_XML_VALUE)
+        public @ResponseBody UserRepository mapJsonToXML(@RequestBody final UserRepository request) {
+            return request;
+        }
+    }
     @Autowired
     private UserRepository userRepository;
 
